1f6e30b7077183b84b27fdc33d23e120d89ce8e1
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-09-25 09:31:49 +0300

    Update Iteration 3

---
+++ frontend/src/pages/useLogin.js
@@ -0,0 +1,44 @@
+// src/hooks/useLogin.js
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const useLogin = (setIsAuthenticated) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const login = async (email, password) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const user = await response.json();
+        localStorage.setItem("user", JSON.stringify(user));
+        console.log("User logged in successfully!");
+        setIsAuthenticated(true);
+        navigate("/");
+      } else {
+        console.error("Login failed");
+        setError("Login failed. Please check your credentials.");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      setError("An unexpected error occurred. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { login, loading, error };
+};
+
+export default useLogin;
